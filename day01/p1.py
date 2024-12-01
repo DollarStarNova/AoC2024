c57d75a2546e3edcf9d69935a37d83996bd6aac6
James <=>
2024-12-01 21:09:57 +0000

    day one complete
    Signed-off-by: James <4439246+DollarStarNova@users.noreply.github.com>

---
+++ day01/p1.py
@@ -0,0 +1,18 @@
+list_a = []
+list_b = []
+diffsum = 0
+
+with open("input.txt", "r") as data:
+    for line in data:
+        a, b = line.split()
+        list_a.append(int(a))
+        list_b.append(int(b))
+list_a.sort()
+list_b.sort()
+
+i=0
+while i < len(list_a):
+    diffsum += abs(list_a[i] - list_b[i])
+    i+=1
+
+print(diffsum)
---
+++ day01/p2.py
@@ -0,0 +1,32 @@
+list_a = []
+list_b = []
+diffsum = 0
+
+with open("input.txt", "r") as data:
+    for line in data:
+        a, b = line.split()
+        list_a.append(int(a))
+        list_b.append(int(b))
+list_a.sort()
+list_b.sort()
+
+entry_counter = {}
+for a in list_a:
+    if not str(a) in entry_counter.keys():
+        entry_counter[str(a)] = {'leftcount':1, 'rightcount':0}
+    else:
+        entry_counter[str(a)] = {
+            'leftcount': entry_counter[str(a)]['leftcount'] + 1,
+            'rightcount': entry_counter[str(a)]['rightcount']
+        }
+
+for b in list_b:
+    if str(b) in entry_counter.keys():
+        entry_counter[str(b)]['rightcount'] = entry_counter[str(b)]['rightcount'] + 1
+
+total = 0
+
+for key, value in entry_counter.items():
+    total += value['leftcount'] * int(key) * value['rightcount']
+
+print(total)
